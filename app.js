1b3c82236b8e113970f9286d72fe6be6a516a53f
yueCom <yuezy3@zoho.com>
2017-09-26 17:12:40 +0800

    using prism and highlight, error in it.

--- app.js
+++ app.js
@@ -1,29 +1,57 @@
 const http = require('http');
+const fs   = require("fs");
 const cycle = require('./cycle.js');
 const express = require('express');
-const mk = require("./mkdown.js")
+const hljs = require("highlight.js");
 const hostname = '127.0.0.1';
 const port = 3000;
 
 
 app = express();
 // =============================================================
-let handleFunc = (req, res) => {
+let handleFunc = (req, res, next) => {
     res.statusCode = 200;
-    // res.setHeader('Content-Type', 'text/plain');
-    // let reqJOSN = {};
-    // Object.keys(req).map((reqkey)=>{reqJOSN[reqkey]=req[reqkey];});
-    // res.write(JSON.stringify(cycle.decycle(req)));
-    res.write(mk.markd)
-    res.write('Hello World\n');
-  }
+    res.setHeader('Content-Type', 'text/html');
+    let MarkDownIt = require('markdown-it');
+    let mk = require('markdown-it-katex');
+    let md = new MarkDownIt({html:true});
+    md.use(mk);
+    console.log("begain to handle xxx");
+    fs.readFile('test.md','utf-8',function(err,data){
+      console.log("begain to read file");
+      if(err){console.log(err);res.end("end with error");}
+      else {
+        let result = md.render(data);
+        fs.writeFileSync('data.html',result);
+        res.end(result);
+      }
+    });
+    next();
+  };
 
 // ==============================================================
-app.use(function(req,res,next){
-  handleFunc(req,res);
-  next();
-})
-app.use(function(req,res,next){res.end();})
+app.get("/mktest.html",function(req,res,next){
+  res.statusCode = 200;
+  res.setHeader("Content-Type","text/html");
+  fs.readFile("mktest.html",{encodeing:"utf-8"},function(err,data){
+    res.end(data);
+  });
+});
+app.get("/prism.js",function(req,res,next){
+  res.statusCode = 200;
+  res.setHeader("Content-Type","text/javascript");
+  fs.readFile("prism.js",{encodeing:"utf-8"},function(err,data){
+    res.end(data);
+  });
+});
+app.get("/prism.css",function(req,res,next){
+  res.statusCode = 200;
+  res.setHeader("Content-Type","text/css");
+  fs.readFile("prism.css",{encodeing:"utf-8"},function(err,data){
+    res.end(data);
+  });
+});
+app.use(handleFunc);
 const server = http.createServer(app);
 
 
--- mkdown.js
+++ mkdown.js
@@ -4,7 +4,7 @@ var mk = require('markdown-it-katex');
 var md = new MarkDownIt();
 md.use(mk);
 var str = fs.readFileSync('test.md','utf-8');
-console.log(str);
+
 var result = md.render(str);
 
 
